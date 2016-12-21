c9912a4143f334c17b2fb3ffe2064b0644f05da1
Dan Rice <dan.rice.92@outlook.com>
2016-12-21 08:23:59 -0700

    2: List Chat Rooms completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,12 +4,14 @@
             .html5Mode({
                 enabled: true,
                 requireBase: false
-        })
+            });
+        
         $stateProvider
             .state('home', {
                 url: '/',
+                controller: 'HomeCtrl as home',
                 templateUrl: '/templates/home.html'
-        });
+            });
     }
     
     angular
